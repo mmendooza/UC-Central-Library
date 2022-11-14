cd02798c9e641229104c1ac0af9df160ff5e9f85
supernaudy <naudy94@gmail.com>
2022-11-13 22:40:50 -0400

    [ADD] uc_central_library

---
+++ uc_central_library/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "UC Central Library",
+	'version': '1.0',
+	'description': """""",
+	'category': '',
+	'author': 'Fantastic Four',
+	'depends': ['base'],
+	'data': [],
+	"license": "LGPL-3",
+}
