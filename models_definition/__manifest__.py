161ca91dd373f83d9b02867eeca5df3eb05e989d
mm3nd0za <mmndza30@gmail.com>
2022-11-14 00:13:05 -0400

    [ADD]: models_definition module

---
+++ models_definition/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Models definition",
+	'summary': """Centralized definition of the set of models that make up the UC library card system.""",
+	'author': "FACyT / Computer science",
+	'website': "http://www.facyt.uc.edu.ve/",
+	'category': 'Technical',
+	'depends': ['base'],
+	'data': [
+		'security/ir.model.access.csv',
+		'views/models_definition_views.xml',
+		'views/models_definition_actions.xml',
+		'views/models_definition_menu.xml',
+	],
+	'license': 'Other proprietary',
+}
