283e2b05d4dc4a764fa6536c6c4fc9d2b321878c
mm3nd0za <mmndza30@gmail.com>
2022-11-15 00:58:34 -0400

    [IMP]: Fields to Information Unit model

--- models_definition/models/information_unit.py
+++ models_definition/models/information_unit.py
@@ -8,4 +8,13 @@ class InformationUnit(models.Model):
 	_description = "Libraries, information centers, documentation centers, archives are information units where users \
     can access information services."
 
+	_sql_constraints = [('code_uniq', 'unique (code)', 'Unit code must be unique!')]
+
 	name = fields.Char(string='Name')
+	code = fields.Char(string="Code")
+	#faculty_id = fields.Many2one(comodel_name="", string="Faculty")
+	library_name = fields.Char(string="Library name")
+	address = fields.Char(string="Address")
+	website = fields.Char(string="Website Link")
+	email = fields.Char(string="Email")
+	initials = fields.Char(string="Initials")
