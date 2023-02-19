d339c80aaba418cfb819106c1537d80292ec3e02
angelicastro <angelicastro1997@gmail.com>
2023-02-19 18:44:42 -0400

    faculty, mention and school masters

--- models_definition/models/mention.py
+++ models_definition/models/mention.py
@@ -3,13 +3,10 @@ from odoo import models, api, fields, _
 class Mention(models.Model):
     _name = 'mention'
     _description = "Mention"
+    _rec_name = "menc_nomb"
 
-    menc_escu = fields.Many2one('school',string="Escuela", required=True)
-    menc_codi = fields.Char(string="Código", required=True)
     menc_nomb = fields.Char(string="Nombre", required=True)
-    menc_obse = fields.Char(string="No sé")
+    menc_escu = fields.Many2one(comodel_name="school",string="Escuela", required=True)
+    menc_codi = fields.Char(string="Código", required=True)
     menc_nume = fields.Char(string="Número")
-    # menc_crea
-    # menc_cusu
-    # menc_modi
-    # menc_musu
+    menc_obse = fields.Char(string="No sé menc_obse")
--- models_definition/models/school.py
+++ models_definition/models/school.py
@@ -3,20 +3,17 @@ from odoo import models, api, fields, _
 class School(models.Model):
     _name = 'school'
     _description = "School"
+    _rec_name = 'escu_nomb'
 
-    escu_auto = fields.Char(string="No sé")
-    escu_codi = fields.Char(string="Código", required=True)
-    escu_facu = fields.Many2one('faculty',string="Facultad")
-    escu_nomb = fields.Char(string="Nombre")
-    escu_nume = fields.Char(string="Código de control de estudio")
+    escu_nomb = fields.Char(string="Nombre", required=True)
     escu_dire = fields.Char(string="Dirección")
+    escu_codi = fields.Char(string="Código", required=True)
+    escu_facu = fields.Many2one(comodel_name="faculty",string="Facultad", required=True)
+    escu_nume = fields.Char(string="Código de control de estudio", required=True)
+    escu_auto = fields.Char(string="No sé escu_auto")
     escu_emai = fields.Char(string="Correo")
     escu_url = fields.Char(string="Sitio web")
-    escu_cnu = fields.Char(string="No sé")
-    escu_obse = fields.Char(string="No sé")
+    escu_cnu = fields.Char(string="No sé escu_cnu")
+    escu_obse = fields.Char(string="No sé escu_obse")
     escu_logo = fields.Image(string='Logo', max_width=500, max_height=500)
-    escu_sigl = fields.Char(string="No sé")
-    # escu_crea 
-    # escu_cusu
-    # escu_modi
-    # escu_musu
+    escu_sigl = fields.Char(string="No sé escu_sigl")
