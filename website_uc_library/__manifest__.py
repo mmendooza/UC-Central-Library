692adf061b32b466babd234e90566261e513ca17
angelicastro <angelicastro1997@gmail.com>
2023-11-13 22:26:33 -0400

    [ADD]: website_uc_library module

---
+++ website_uc_library/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Theme UC Library',
+    'description': 'Default website theme',
+    'category': 'Theme',
+    'sequence': 1000,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/header.xml',
+        'views/homepage.xml',
+        #'views/footer.xml',
+    ],
+    # 'assets':{
+    #     'web.assets_frontend': [
+    #         'website_uc_library/static/src/scss/styles.scss',
+    #     ],
+    # },
+    'images': [
+    ],
+    'snippet_lists': {
+    },
+    'license': 'LGPL-3',
+}
